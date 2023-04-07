d70727e137748b91c355f71104c681787422eb53
renauga <saumya_t@cs.iitr.ac.in>
2023-04-07 17:19:36 +0530

    added trace to template

--- Template/template yatin.cpp
+++ Template/template yatin.cpp
@@ -1,24 +1,105 @@
 #include <bits/stdc++.h>
-
+using namespace std;
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-
-using namespace std;
 using namespace __gnu_pbds;
 
 template <typename T>
-using ordered_set =
-    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-#define all(x) x.begin(), x.end()
-#define fix(f, n) std::fixed << std::setprecision(n) << f
-#define start_clock()                                     \
-  auto start_time = chrono::high_resolution_clock::now(); \
-  auto end_time = start_time;
-#define measure()                                                        \
-  end_time = chrono::high_resolution_clock::now();                       \
-  cerr << (end_time - start_time) / std::chrono::milliseconds(1) << "ms" \
-       << endl;
+//use only if code is very short
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+
+#define start_clock() auto start_time = std::chrono::high_resolution_clock::now();
+#define measure() auto end_time = std::chrono::high_resolution_clock::now(); cerr << (end_time - start_time)/std::chrono::milliseconds(1) << "ms" << endl;
+
+typedef long long ll;
+
+// DEBUG FUNCTIONS 
+#ifndef ONLINE_JUDGE
+
+template<typename T>
+void __p(T a) {
+	cout<<a;
+}
+template<typename T, typename F>
+void __p(pair<T, F> a) {
+	cout<<"{";
+	__p(a.first);
+	cout<<",";
+	__p(a.second);
+	cout<<"}";
+}
+template<typename T>
+void __p(std::vector<T> a) {
+	cout<<"{";
+	for(auto it=a.begin(); it<a.end(); it++)
+		__p(*it),cout<<",}"[it+1==a.end()];
+}
+template<typename T>
+void __p(std::set<T> a) {
+	cout<<"{";
+	for(auto it=a.begin(); it!=a.end();){
+		__p(*it); 
+		cout<<",}"[++it==a.end()];
+	}
+
+}
+template<typename T>
+void __p(std::multiset<T> a) {
+	cout<<"{";
+	for(auto it=a.begin(); it!=a.end();){
+		__p(*it); 
+		cout<<",}"[++it==a.end()];
+	}
+}
+template<typename T, typename F>
+void __p(std::map<T,F> a) {
+	cout<<"{\n";
+	for(auto it=a.begin(); it!=a.end();++it)
+	{
+		__p(it->first);
+		cout << ": ";
+		__p(it->second);
+		cout<<"\n";
+	}
+	cout << "}\n";
+}
+
+template<typename T, typename ...Arg>
+void __p(T a1, Arg ...a) {
+	__p(a1);
+	__p(a...);
+}
+template<typename Arg1>
+void __f(const char *name, Arg1 &&arg1) {
+	cout<<name<<" : ";
+	__p(arg1);
+	cout<<endl;
+}
+template<typename Arg1, typename ... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&... args) {
+	int bracket=0,i=0;
+	for(;; i++)
+		if(names[i]==','&&bracket==0)
+			break;
+		else if(names[i]=='(')
+			bracket++;
+		else if(names[i]==')')
+			bracket--;
+	const char *comma=names+i;
+	cout.write(names,comma-names)<<" : ";
+	__p(arg1);
+	cout<<" | ";
+	__f(comma+1,args...);
+}
+#define trace(...) cout<<"Line:"<<__LINE__<<" ", __f(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define trace(...)
+#define error(...)
+#endif
 
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
@@ -37,8 +118,7 @@ struct custom_hash {
 };
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-
-  return 0;
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	return 0;
 }
